88e30031db8252c07e4ca8bb17a47b011d446abb
DaniilSamoilov <danya.sam2@yandex.ru>
2023-07-04 10:49:40 +0300

    Добавьте файлы проекта.

---
+++ 15/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _15
+{
+    public partial class MainWindow : Window
+    {
+        List<element> elements= new List<element>();
+        bool[,] positions =new bool[4, 4];
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            positions = generatePositions();
+            CreateElements();
+        }
+
+        void CreateElements()
+        {
+            int i = 1;
+
+            var result = createSolvableSequence();
+            foreach (var item in result)
+            {
+                elements.Add(new element(ref MainGrid, item, i / 4, i % 4,ref positions));
+                i++;
+            }
+            foreach (var item in elements)
+            {
+                item.btn.Click += checkWin;
+            }
+        }
+        bool[,] generatePositions()
+        {
+            int EmptyTile = new Random().Next(1,17);
+            for (int i = 0; i < 16; i++)
+            {
+                if (0 ==i)
+                {
+                    this.positions[i / 4, i % 4] = false;
+                }
+                else
+                {
+                    this.positions[i / 4, i % 4] = true;
+                }
+            }
+            return positions;
+        }
+
+        void checkWin(object sender,EventArgs e)
+        {
+            int[] Order =  new int[16];
+            foreach (var item in elements)
+            {
+                if (item.tag!=(item.x_pos_temp*4+item.y_pos_temp).ToString())
+                {
+                    return;
+                }
+            }
+            MessageBox.Show("Победа");
+        }
+
+        int[] createSolvableSequence()
+        {
+            int[] numbers = new int[15];
+            bool check = true;
+            var result = Enumerable.Range(1, 15).OrderBy(g => Guid.NewGuid()).ToArray();
+            while (check)
+            {
+                result = Enumerable.Range(1, 15).OrderBy(g => Guid.NewGuid()).ToArray();
+                int temp = 0;
+                foreach (var item in result)
+                {
+                    numbers[temp] = item;
+                    temp++;
+                }
+                int inv = 0;
+                for (int i = 0; i < 15; ++i)
+                {
+                    for (int j = 0; j < i; ++j)
+                    {
+                        if (numbers[j] > numbers[i]) { ++inv; }
+                    }
+                }
+                if (inv % 2 == 0){check = false;}
+            }
+            return result;
+        }
+    }
+}
---
+++ 15/element.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Net.Http;
+
+namespace _15
+{
+    internal class element
+    {
+        int x_pos;
+        public int x_pos_temp;
+        int y_pos;
+        public int y_pos_temp;
+        public string tag;
+        public Button btn;
+        bool[,] positions;
+
+        public element(ref Grid GridToAdd, int tag, int x_pos, int y_pos, ref bool[,] positions)
+        {
+            this.x_pos = x_pos;
+            this.y_pos = y_pos;
+            this.tag = tag.ToString();
+            this.btn = new Button();
+            this.positions = positions;
+            btn.SetValue(Grid.RowProperty, x_pos);
+            btn.SetValue(Grid.ColumnProperty, y_pos);
+            btn.Content = tag;
+            btn.Click += move;
+            GridToAdd.Children.Add(btn);
+        }
+        private void update(object sender,EventArgs e)
+        {
+            btn.RenderTransform = null;
+            btn.SetValue(Grid.RowProperty, x_pos);
+            btn.SetValue(Grid.ColumnProperty, y_pos);
+        }
+        private void move(object sender, RoutedEventArgs e)
+        {
+            checkAllNeighbours();
+            animateMove();
+        }
+        private void checkAllNeighbours()
+        {
+            copyCoordinats();
+            if (y_pos + 1 <= positions.GetLength(0)-1 && !positions[x_pos, y_pos + 1])
+            {
+                positions[x_pos, y_pos + 1] = true;
+                positions[x_pos, y_pos] = false;
+                y_pos++;
+                return;
+            }
+
+            if (y_pos - 1 >= 0 && !positions[x_pos, y_pos - 1])
+            {
+                positions[x_pos, y_pos - 1] = true;
+                positions[x_pos, y_pos] = false;
+                y_pos--;
+                return;
+            }
+
+            if (x_pos + 1 <= positions.GetLength(0) - 1 && !positions[x_pos + 1, y_pos])
+            {
+                positions[x_pos + 1, y_pos] = true;
+                positions[x_pos, y_pos] = false;
+                x_pos++;
+                return;
+            }
+
+            if (x_pos - 1 >= 0 && !positions[x_pos - 1, y_pos])
+            {
+                positions[x_pos - 1, y_pos] = true;
+                positions[x_pos, y_pos] = false;
+                x_pos--;
+                return;
+            }
+        }
+        void copyCoordinats()
+        {
+            x_pos_temp = x_pos;
+            y_pos_temp = y_pos;
+        }
+        void animateMove()
+        {
+            TranslateTransform movement = new TranslateTransform();
+            btn.RenderTransform= movement;
+            
+            DoubleAnimation animy = new DoubleAnimation(0, (x_pos - x_pos_temp) * btn.ActualWidth, TimeSpan.FromSeconds(0.5));
+            animy.Completed += update;
+            DoubleAnimation animx = new DoubleAnimation(0, (y_pos - y_pos_temp) * btn.ActualHeight, TimeSpan.FromSeconds(0.5));
+            animx.Completed += update;
+            movement.BeginAnimation(TranslateTransform.XProperty, animx);
+            movement.BeginAnimation(TranslateTransform.YProperty, animy);
+        }
+    }
+}
